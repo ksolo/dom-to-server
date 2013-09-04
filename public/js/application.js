2895edef1e3c2727e8adf2ee6e960250715ae0d6
Kevin Solorio <soloriok@gmail.com>
2013-09-04 13:55:38 -0500

    solve pt.1

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+  $('form').submit(function(event){
+    event.preventDefault();
+
+    var random = Math.floor(Math.random() * 6) + 1;
+    var data = {value: random};
+    var url = $(this).attr('action');
+
+    $.post(url, data, function(response){
+      $('#die').html(response);
+    });
+  });
+
+
+
+
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
